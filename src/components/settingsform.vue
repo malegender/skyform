e56370e1e9af6cc9a6fe836a4235e80e8377199d
s.aleksandrov@demis.ru <s.aleksandrov@demis.ru>
2017-06-06 19:08:34 +0300

    add fields

--- src/components/settingsform.vue
+++ src/components/settingsform.vue
@@ -35,7 +35,7 @@
                     :key="i"
                     v-else
                 >
-                  <v-list-tile @click.native="add(item.action)">
+                  <v-list-tile @click.native="add(item.entity)">
                     <v-list-tile-action>
                       <v-icon>{{ item.icon }}</v-icon>
                     </v-list-tile-action>
@@ -63,37 +63,67 @@
               <v-flex xs12 md6 offset-md3 class="wrap-content">
                   <draggable v-model="blocks">
                     <transition-group name="list-blocks">
-                      <div v-for="(block, index) in blocks" :key="index" class="sort-block">
-                        <v-btn @click.native.stop="settingBlock(index)"
+                      <div v-for="(block, bindex) in blocks" :key="bindex" class="sort-block">
+                        <v-btn @click.native.stop="settingBlock(bindex)"
                                floating small primary>
                           <v-icon light>settings</v-icon>
                         </v-btn>
                         <v-btn v-if="blocks.length > 1"
-                               @click.native.stop="dialogConfirmOpen('removeBlock',{'index': index, 'message': remove_block_message})"
+                               @click.native.stop="dialogConfirmOpen('removeBlock',{'bindex': bindex, 'message': remove_block_message})"
                                floating small error
                         >
                           <v-icon light>delete</v-icon>
                         </v-btn>
                         <draggable v-model="block.fields" :options="{group:'form'}">
                           <transition-group name="list-blocks-fields">
-                            <v-card v-for="(field, index) in block.fields"
-                                    :key="index"
+                            <v-card v-for="(field, findex) in block.fields"
+                                    :key="findex"
                                     @mouseover="field.buttons=true"
                                     @mouseout="field.buttons=false"
                                     class="sort-field"
                             >
                               <v-layout row wrap>
-                                <v-flex xs5 md9>
-                                  {{ field.name }}
+                                <v-flex xs12>
+                                  <label v-if="field.label">{{ field.label }}</label>
+                                  <pre v-if="field.entity == 'freearea'">{{ field.attributes.value }}</pre>
+                                  <input :type="field.attributes.type"
+                                         :class="{label: field.label, 'error-text': field.error}"
+                                         :value="field.attributes.value"
+                                         :placeholder="field.attributes.placeholder"
+                                         readonly
+                                         v-if="['input', 'email', 'phone', 'checkbox', 'radio'].indexOf(field.entity) > -1"
+                                  >
+                                  <textarea rows="2"
+                                            :class="{label: field.label, 'error-text': field.error}"
+                                            :placeholder="field.attributes.placeholder"
+                                            readonly
+                                            v-if="field.entity == 'textarea'"
+                                  >{{ field.attributes.value }}</textarea>
+                                  <select v-model="field.attributes.value"
+                                          :class="{label: field.label, 'error-text': field.error}"
+                                          :placeholder="field.attributes.placeholder"
+                                          readonly
+                                          v-if="field.entity == 'select'"
+                                  >
+                                    <option v-for="(option, oindex) in field.options"
+                                            :key="oindex"
+                                            :value="option.value ? option.value : option.label"
+                                            :selected="option.value == field.value"
+                                    >{{ option.label }}</option>
+                                  </select>
+                                  <div class="error-text cross" v-if="field.error">{{ field.error }}</div>
                                 </v-flex>
-                                <v-flex xs7 md3 class="text-xs-right" v-show="field.buttons">
+                                <v-flex xs12
+                                        class="text-xs-right block-buttons"
+                                        v-show="field.buttons"
+                                >
                                   <v-btn floating small primary>
                                     <v-icon light>settings</v-icon>
                                   </v-btn>
-                                  <v-btn floating small warning>
+                                  <v-btn @click.native.stop="copyField(bindex, findex)" floating small warning>
                                     <v-icon light>content_copy</v-icon>
                                   </v-btn>
-                                  <v-btn @click.native.stop="removeField(index)" floating small error>
+                                  <v-btn @click.native.stop="removeField(bindex, findex)" floating small error>
                                     <v-icon light>delete</v-icon>
                                   </v-btn>
                                 </v-flex>
@@ -117,7 +147,7 @@
             </v-card-row>
             <v-card-row actions>
               <v-btn class="red--text darken-1" flat="flat" @click.native.stop="dialogConfirm.visible = false">{{ button_cancel_label }}</v-btn>
-              <v-btn class="blue--text darken-1" flat="flat" @click.native.stop="dialogConfirmAction">{{ button_save_label }}</v-btn>
+              <v-btn class="blue--text darken-1" flat="flat" @click.native.stop="dialogConfirmAction">{{ button_continue_label }}</v-btn>
             </v-card-row>
           </v-card>
         </v-dialog>
@@ -160,22 +190,25 @@
         'remove_block_message': 'Будут удалены все поля входящие в эту группу!',
         'button_activator_label': 'Поля формы',
         'button_save_label': 'Сохранить',
+        'button_continue_label': 'Продолжить',
         'button_cancel_label': 'Отменить',
         'drawer': true,
         'mini': true,
         'menu': [
           {'heading': 'Блоки'},
-          {'icon': 'view_quilt', 'text': 'Группа полей', 'action': 'block'},
-          {'icon': 'send', 'text': 'Кнопка отправки', 'action': 'submit'},
-          {'icon': 'error_outline', 'text': 'Блок ошибок', 'action': 'error'},
+          {'icon': 'view_quilt', 'text': 'Группа полей', 'entity': 'block'},
+          {'icon': 'send', 'text': 'Кнопка отправки', 'entity': 'submit'},
+          {'icon': 'error_outline', 'text': 'Блок ошибок', 'entity': 'error'},
           {'divider': true},
           {'heading': 'Поля'},
-          {'icon': 'code', 'text': 'Произвольный код', 'action': 'freearea'},
-          {'icon': 'input', 'text': 'Текстовое поле', 'action': 'input'},
-          {'icon': 'reorder', 'text': 'Многострочное текстовое поле', 'action': 'textarea'},
-          {'icon': 'check_box', 'text': 'Флажок', 'action': 'checkbox'},
-          {'icon': 'radio_button_checked', 'text': 'Переключатель', 'action': 'radio'},
-          {'icon': 'list', 'text': 'Раскрывающийся список', 'action': 'select'}
+          {'icon': 'code', 'text': 'Произвольный код', 'entity': 'freearea'},
+          {'icon': 'input', 'text': 'Текстовое поле', 'entity': 'input'},
+          {'icon': 'email', 'text': 'Текстовое поле "E-mail"', 'entity': 'email'},
+          {'icon': 'phone', 'text': 'Текстовое поле "Телефон"', 'entity': 'phone'},
+          {'icon': 'reorder', 'text': 'Многострочное текстовое поле', 'entity': 'textarea'},
+          {'icon': 'check_box', 'text': 'Флажок', 'entity': 'checkbox'},
+          {'icon': 'radio_button_checked', 'text': 'Переключатель', 'entity': 'radio'},
+          {'icon': 'list', 'text': 'Раскрывающийся список', 'entity': 'select'}
         ],
         'blocks': [
           {
@@ -184,18 +217,52 @@
               {'label': 'class', 'model': ''}
             ],
             'fields': [
-              {'name': 'input', 'buttons': false},
-              {'name': 'textarea', 'buttons': false}
-            ]
-          },
-          {
-            'tag': 'div',
-            'attributes': [
-              {'label': 'class', 'model': ''}
-            ],
-            'fields': [
-              {'name': 'input2', 'buttons': false},
-              {'name': 'textarea2', 'buttons': false}
+              {
+                'entity': 'input',
+                'attributes': {
+                  'type': 'text',
+                  'placeholder': 'Как к Вам обращаться?'
+                },
+                'label': 'Ваше имя',
+                'error': 'Поле заполнено не корректно!',
+                'container': {
+                  'tag': 'div',
+                  'attributes': {}
+                },
+                'buttons': false
+              },
+              {
+                'entity': 'textarea',
+                'attributes': {
+                  'placeholder': 'Оставьте Ваше сообщение'
+                },
+                'label': 'Комментарий',
+                'error': 'Поле заполнено не корректно!',
+                'container': {
+                  'tag': 'div',
+                  'attributes': {}
+                },
+                'buttons': false
+              },
+              {
+                'entity': 'select',
+                'label': 'Выберите город',
+                'error': 'Город не выбран!',
+                'attributes': {
+                  'placeholder': 'Выбрать...',
+                  'value': 36
+                },
+                'options': [
+                  {'label': 'Тамбов', 'value': 68},
+                  {'label': 'Липецк', 'value': 48},
+                  {'label': 'Воронеж', 'value': 36}
+                ],
+                'container': {
+                  'tag': 'div',
+                  'attributes': {}
+                },
+                'buttons': false
+              }
             ]
           }
         ]
@@ -213,27 +280,52 @@
       })
     },
     methods: {
-      add (field) {
-        switch (field) {
+      add (entity) {
+        let field = {
+          'entity': entity,
+          'attributes': {},
+          'container': {
+            'tag': 'div',
+            'attributes': {}
+          },
+          'buttons': false
+        }
+        switch (entity) {
           case 'block':
             this.blocks.push({
               'tag': 'div',
               'attributes': [],
               'fields': []
             })
-            break
-          case 'freearea':
-            break
+            return
           case 'input':
-            this.blocks[this.blocks.length - 1]['fields'].push({'name': 'input', 'buttons': false})
+            field.attributes.type = 'text'
+            field.attributes.name = this.generatorName()
+            break
+          case 'email':
+            field.attributes.type = 'text'
+            field.attributes.name = this.generatorName()
+            field.attributes.pattern = /^([a-z,._,.\-,0-9])+@([a-z,._,.\-,0-9])+(\.([a-z])+)+$/
+            break
+          case 'phone':
+            field.attributes.type = 'text'
+            field.attributes.name = this.generatorName()
+            field.attributes.pattern = /^\+?[\d,\-,(,),\s]+$/
+            break
+          case 'checkbox':
+            field.attributes.type = 'checkbox'
+            field.attributes.name = this.generatorName()
+            break
+          case 'radio':
+            field.attributes.type = 'radio'
+            field.attributes.name = this.generatorName()
             break
           case 'textarea':
-            this.blocks[this.blocks.length - 1]['fields'].push({'name': 'textarea', 'buttons': false})
-            break
           case 'select':
-            this.blocks[this.blocks.length - 1]['fields'].push({'name': 'select', 'buttons': false})
+            field.attributes.name = this.generatorName()
             break
         }
+        this.blocks[this.blocks.length - 1]['fields'].push(field)
       },
       dialogConfirmOpen (action, data) {
         this.dialogConfirm.visible = true
@@ -244,17 +336,38 @@
         this[this.dialogConfirm.action](this.dialogConfirm.data)
         this.dialogConfirm.visible = false
       },
-      settingBlock (index) {
+      settingBlock (bindex) {
         if (!this.dialogSettingBlock.visible) {
-          this.dialogSettingBlock.data = this.blocks[index]
+          this.dialogSettingBlock.data = this.blocks[bindex]
         }
         this.dialogSettingBlock.visible = !this.dialogSettingBlock.visible
       },
       removeBlock (data) {
-        this.blocks.splice(data.index, 1)
+        this.blocks.splice(data.bindex, 1)
       },
-      removeField (index) {
-        alert(index)
+      removeField (bindex, findex) {
+        this.blocks[bindex]['fields'].splice(findex, 1)
+      },
+      copyField (bindex, findex) {
+        let field = _.cloneDeep(this.blocks[bindex]['fields'][findex])
+        if (field.attributes && field.attributes.name) field.attributes.name = this.generatorName()
+        this.blocks[bindex]['fields'].splice(findex, 0, field)
+      },
+      generatorName () {
+        let name = `field-${(((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1)}`
+        let double = false
+
+        _.forEach(this.blocks, function (block) {
+          _.forEach(block.fields, function (field) {
+            if (field.attributes && field.attributes.name && field.attributes.name === name) {
+              double = true
+            }
+          })
+        })
+
+        if (double) name = this.generatorName()
+
+        return name
       }
     }
   }
